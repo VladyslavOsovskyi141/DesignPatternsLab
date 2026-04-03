bfe5ee77f440602cc386eb7922afc482609d243c
VladislavOsovskyi141 <osovskyi.vladyslav@chnu.edu.ua>
2026-04-03 12:01:46 +0300

    Апдейтнуто Main.java

--- src/creational/Main.java
+++ src/creational/Main.java
@@ -2,28 +2,28 @@ package creational;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("--- 1. Singleton ---");
+        System.out.println("1. Singleton:");
         SingletonExample singleton = SingletonExample.getInstance();
         singleton.sayHello(); // Виведе "Hello"
 
-        System.out.println("\n--- 2. Factory ---");
+        System.out.println("\n2. Factory:");
         AnimalFactory animalFactory = new AnimalFactory();
         Animal cat = animalFactory.getAnimal("feline");
         System.out.println("Cat sound: " + cat.makeSound());
 
-        System.out.println("\n--- 3. Abstract Factory ---");
+        System.out.println("\n3. Abstract Factory:");
         AbstractFactory abstractFactory = new AbstractFactory();
         SpeciesFactory reptileFactory = abstractFactory.getSpeciesFactory("reptile");
         Animal snake = reptileFactory.getAnimal("snake");
         System.out.println("Snake sound: " + snake.makeSound());
 
-        System.out.println("\n--- 4. Builder ---");
+        System.out.println("\n4. Builder:");
         MealBuilder builder = new ItalianMealBuilder();
         MealDirector director = new MealDirector(builder);
         director.constructMeal();
         System.out.println("Italian meal: " + director.getMeal());
 
-        System.out.println("\n--- 5. Prototype ---");
+        System.out.println("\n5. Prototype:");
         Person originalPerson = new Person("Fred");
         Person clonedPerson = (Person) originalPerson.doClone();
         System.out.println("Original: " + originalPerson + " | Clone: " + clonedPerson);
